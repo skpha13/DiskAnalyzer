e4efb3cacd791fdc7b6e00cf4a398419d535fd78
FIRCALiviu <liviu.firca@gmail.com>
2023-11-29 13:43:27 +0200

    added tasks and thread struct

---
+++ da.c
@@ -0,0 +1,24 @@
+#include<string.h>
+
+int main(int argc ,char** argv){
+
+    
+
+
+
+
+}
+
+
+/*
+--help display usage message  T
+-a, --add analyze a new directory path for disk usage L
+-p, --priority set priority for the new analysis (works only with -a argument) T
+-S, --suspend <id> suspend task with <id> T
+-R, --resume <id> resume task with <id> L
+-r, --remove <id> remove the analysis with the given <id> T
+-i, --info <id> print status about the analysis with <id> (pending, progress, d  T
+-l, --list list all analysis tasks, with their ID and the corresponding root p L
+-p, --print <id> print analysis report for those tasks that are "done" T
+
+*/
