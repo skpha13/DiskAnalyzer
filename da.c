2b1f6ef9d33343abe6b6507319eb57c6515edd13
FIRCALiviu <liviu.firca@gmail.com>
2023-11-30 10:49:38 +0200

    partially added user input handling

--- da.c
+++ da.c
@@ -2,12 +2,64 @@
 #include<pthread.h>
 #include<stdio.h>
 
+int is_digit(const char c){
+    switch(c){
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+        case '8':
+        case '9':
+        return 1;
+        default:
+        return 0;
+    }
+}
+
+int is_natural(const char* c){
+    if(!*c) return 0;//handle empty string
+
+    while (!c)
+    {
+        if(!is_digit(*c)) return 0;
+        c++;
+        
+    }
+
+    return 1;
+
+}
+
 int suspend(int id);
 
+
+
+void help();
+
+void listall();
 int main(int argc ,char** argv){
 
+    if(argc==1) {
+        help();
+        return 0;
+    }
 
+    if(argc<=2){//doar da -l este correct sintactic aici
+        if(!strcmp("-l",argv[1])||strcmp("--list",argv[1])){
+            listall();
+            return 0;
+        }
+        help();
+        return 0;
 
+    }
+
+    if(argc<=3){ //doar da -S <id>, da -R <id>, etc... sunt prompturi corecte aici
+        
+    }
 
 
 
