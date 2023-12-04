7ff9caff759346652c407362b9a21d84b60a39c3
FIRCALiviu <liviu.firca@gmail.com>
2023-12-04 13:34:40 +0200

    finished IPC in daemon

--- DaemonProcess.c
+++ DaemonProcess.c
@@ -13,6 +13,10 @@
 #define THREAD_POOL_SIZE 5
 #define SHARED_MEMORY_NAME "/DiskAnalizerSHM"
 
+int handle_prompt(daemon_file_t * file){
+	return -1;
+}
+
 
 int main(){
 pthread_t thread_pool[THREAD_POOL_SIZE];
@@ -37,12 +41,48 @@ if ( ftruncate ( shared_memory, shared_memory_size ) == -1) {
     }
 
 daemon_file_t* communication_file=mmap(0,shared_memory_size,PROT_READ|PROT_WRITE,MAP_SHARED,shared_memory,0);
+//daemon initialises sync devices
+if(pthread_mutex_init(&communication_file->acces_file,NULL)){
+	perror(NULL);
+	syslog(LOG_INFO,FORMAT_LOG_ERROR,errno);
+	closelog();
+	return EXIT_FAILURE;
+}
 
-	
 
-/*to do: add instruction queue*/
+if(pthread_mutex_init(&communication_file->shell_wait,NULL)){
+	perror(NULL);
+	syslog(LOG_INFO,FORMAT_LOG_ERROR,errno);
+	closelog();
+	return EXIT_FAILURE;
+}	
+
+if(sem_init(&communication_file->shell_continue,0,0)){
+	perror(NULL);
+	syslog(LOG_INFO,FORMAT_LOG_ERROR,errno);
+	closelog();
+	return EXIT_FAILURE;
+}
+communication_file->error=0;
+
+
+
 while (1)
 {
+	/*IPC with da shell*/
+	pthread_mutex_lock(&communication_file->acces_file);
+	if(communication_file->task_id!=0){
+		handle_prompt(communication_file);
+		communication_file->task_id=0;
+		if(sem_post(&communication_file->shell_continue)){
+			perror(NULL);
+			syslog(LOG_INFO,FORMAT_LOG_ERROR,errno);
+			closelog();
+		}
+
+	}
+	pthread_mutex_unlock(&communication_file->acces_file);
+
 	sleep(1);
 
 }
